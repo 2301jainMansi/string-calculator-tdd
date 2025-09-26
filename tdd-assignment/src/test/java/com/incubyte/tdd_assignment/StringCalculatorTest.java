81a78bc6833703be1f7c1a3b38da9625d9711814
mansi <mansi@SIPL-LPT-118.SIPL.WORK>
2025-09-26 21:55:24 +0530

    - Empty string returns 0 - Single number returns itself - Two or more comma-separated numbers - Multiple numbers - Newlines as delimiter - Custom delimiter - Negative numbers throw exception

--- tdd-assignment/src/test/java/com/incubyte/tdd_assignment/StringCalculatorTest.java
+++ tdd-assignment/src/test/java/com/incubyte/tdd_assignment/StringCalculatorTest.java
@@ -10,11 +10,39 @@ import com.incubyte.tdd.calculator.StringCalculator;
 public class StringCalculatorTest {
 
 	@Test
+	void emptyStringReturnsZero() {
+	    assertEquals(0, new StringCalculator().add(""));
+	}
+
+	@Test
+	void singleNumberReturnsItself() {
+	    assertEquals(5, new StringCalculator().add("5"));
+	}
+
+	@Test
+	void twoNumbersCommaSeparatedReturnsSum() {
+	    assertEquals(3, new StringCalculator().add("1,2"));
+	}
+
+	@Test
+	void multipleNumbersCommaSeparatedReturnsSum() {
+	    assertEquals(10, new StringCalculator().add("1,2,3,4"));
+	}
+
+	@Test
+	void newlinesBetweenNumbersAreValid() {
+	    assertEquals(6, new StringCalculator().add("1\n2,3"));
+	}
+
+	@Test
+	void customDelimiterReturnsSum() {
+	    assertEquals(3, new StringCalculator().add("//;\n1;2"));
+	}
+
+	@Test
 	void negativeNumbersThrowException() {
-	    StringCalculator calc = new StringCalculator();
-	    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-	        calc.add("1,-2,3,-4");
-	    });
-	    assertEquals("Negative numbers not allowed: -2, -4", exception.getMessage());
+	    Exception ex = assertThrows(IllegalArgumentException.class,
+	        () -> new StringCalculator().add("1,-2,3,-4"));
+	    assertEquals("Negative numbers not allowed: -2, -4", ex.getMessage());
 	}
 }
