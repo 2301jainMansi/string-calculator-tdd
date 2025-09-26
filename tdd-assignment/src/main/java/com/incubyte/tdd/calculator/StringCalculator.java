4ce4cdab9a5e4242b2f886dcaf933a8ad141fd09
mansi <mansi@SIPL-LPT-118.SIPL.WORK>
2025-09-26 21:19:24 +0530

    feat: support single number input in add()

--- tdd-assignment/src/main/java/com/incubyte/tdd/calculator/StringCalculator.java
+++ tdd-assignment/src/main/java/com/incubyte/tdd/calculator/StringCalculator.java
@@ -1,10 +1,10 @@
 package com.incubyte.tdd.calculator;
 
 public class StringCalculator {
-	
 	public int add(String numbers) {
-		
-		return 0;
+	    if (numbers.isEmpty()) {
+	        return 0;
+	    }
+	    return Integer.parseInt(numbers);
 	}
-
 }
