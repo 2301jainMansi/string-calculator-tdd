cec7cf723652e347bd5dd93d6efc39ad988317a6
mansi <mansi@SIPL-LPT-118.SIPL.WORK>
2025-09-26 21:43:00 +0530

    Support newline as delimiter in add()

--- tdd-assignment/src/main/java/com/incubyte/tdd/calculator/StringCalculator.java
+++ tdd-assignment/src/main/java/com/incubyte/tdd/calculator/StringCalculator.java
@@ -3,6 +3,8 @@ package com.incubyte.tdd.calculator;
 public class StringCalculator {
 	public int add(String numbers) {
 	    if (numbers.isEmpty()) return 0;
+	    
+	    numbers = numbers.replace("\n", ",");
 	    String[] nums = numbers.split(",");
 	    int sum = 0;
 	    for (String num : nums) {
